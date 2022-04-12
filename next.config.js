a3b120db08347c4dfebdd252cddbb62fd6912e7a
fowlerro <fwlerro@gmail.com>
2022-04-12 16:44:04 +0200

    change API_URL

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextTranslate = require('next-translate');
 module.exports = nextTranslate({
 	reactStrictMode: true,
 	env: {
-		// API_URL: 'https://api.lunaris.pro/api',
-		API_URL: 'http://192.168.0.103:3001/api',
+		API_URL: 'https://api.lunaris.pro/api',
+		// API_URL: 'http://192.168.0.103:3001/api',
 		INVITE_URL:
 			'https://discord.com/api/oauth2/authorize?client_id=739412828737372181&permissions=8&redirect_uri=http%3A%2F%2F192.168.0.103%3A3000%2Fapi%2Finvite%2Fredirect&response_type=code&scope=identify%20bot%20applications.commands',
 	},
