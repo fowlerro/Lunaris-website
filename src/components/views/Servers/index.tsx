e5a091950176510365f4138ffa1ec487b6f13764
fowlerro <fwlerro@gmail.com>
2022-05-06 20:42:38 +0200

    fix #19

--- src/components/views/Servers/index.tsx
+++ src/components/views/Servers/index.tsx
@@ -5,8 +5,10 @@ import ServersSection from './ServersSection';
 import { fetcher } from '@utils/utils';
 
 import type { MutualGuilds } from 'types';
+import useUser from '@hooks/useUser';
 
 export default function Servers(): JSX.Element {
+	useUser({ redirectTo: '/' });
 	const { data: guilds } = useSWR<MutualGuilds>(`${process.env.NEXT_PUBLIC_API_URL}/guilds`, fetcher);
 
 	return (
