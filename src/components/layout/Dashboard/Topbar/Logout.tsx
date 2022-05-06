6d4ab35480e58ce0a05f33fea8c4751bd87e9421
fowlerro <fwlerro@gmail.com>
2022-05-06 21:24:07 +0200

    fix #17

--- src/components/layout/Dashboard/Topbar/Logout.tsx
+++ src/components/layout/Dashboard/Topbar/Logout.tsx
@@ -1,4 +1,6 @@
-import { IconButton } from '@mui/material';
+import useTranslation from 'next-translate/useTranslation';
+
+import { IconButton, Tooltip } from '@mui/material';
 
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,11 +8,14 @@ import Icon from '@components/Icon';
 import Link from '@components/Link';
 
 export default function Logout(): JSX.Element {
+	const { t } = useTranslation('layout');
 	return (
-		<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`} sx={{ marginLeft: '1rem' }}>
-			<IconButton color='error'>
-				<Icon icon={faRightToBracket} />
-			</IconButton>
-		</Link>
+		<Tooltip title={t('profileMenu.logout')}>
+			<Link href={`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`} sx={{ marginLeft: '1rem' }}>
+				<IconButton color='error'>
+					<Icon icon={faRightToBracket} />
+				</IconButton>
+			</Link>
+		</Tooltip>
 	);
 }
